35141bc280bfbbfad0b65ddb12a5967c5b19c5bb
Parminder Singh <97108776+Parminder8851@users.noreply.github.com>
2022-01-04 20:25:48 +0530

    Added Code

---
+++ src/GIT_TESt.java
@@ -0,0 +1,18 @@
+import static jdk.internal.logger.DefaultLoggerFinder.SharedLoggers.system;
+
+public class GIT_TESt { public static void main(String[])
+
+
+   int Taste = '5';
+    if (Taste<4);
+
+ {
+  System.out.println('Oh Sorry');
+
+ } else{
+  System.out.println('Enjoy');
+
+
+ }
+ }
+ }
